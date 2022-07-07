9354bef70a9e946e60b1e979b727f9c9bd04a58f
sourab <sourab@updot.in>
2022-07-07 11:50:11 +0530

    Scafolded

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", ".components/**/*.{js,ts,tsx,jsx}"],
+  theme: {
+    letterspacing: {
+      tight: "-.015em",
+    },
+    extend: {
+      height: {
+        "half-screen": "50vh",
+      },
+    },
+  },
+};
